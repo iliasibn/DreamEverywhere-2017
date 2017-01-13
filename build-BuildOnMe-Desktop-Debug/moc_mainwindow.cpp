0ee4437fe268c7ebaeb965a9f2163bf1c4a7a608
Ilias <ibnyahya.ilias@gmail.com>
2017-01-13 17:33:59 +0100

    ALL

--- build-BuildOnMe-Desktop-Debug/moc_mainwindow.cpp
+++ build-BuildOnMe-Desktop-Debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[15];
-    char stringdata0[158];
+    QByteArrayData data[17];
+    char stringdata0[172];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,22 +34,25 @@ QT_MOC_LITERAL(1, 11, 15), // "showImageSignal"
 QT_MOC_LITERAL(2, 27, 0), // ""
 QT_MOC_LITERAL(3, 28, 4), // "play"
 QT_MOC_LITERAL(4, 33, 5), // "pause"
-QT_MOC_LITERAL(5, 39, 11), // "onShowFrame"
-QT_MOC_LITERAL(6, 51, 5), // "frame"
-QT_MOC_LITERAL(7, 57, 8), // "position"
-QT_MOC_LITERAL(8, 66, 12), // "onLineReturn"
-QT_MOC_LITERAL(9, 79, 8), // "timecode"
-QT_MOC_LITERAL(10, 88, 13), // "onSliderMoved"
-QT_MOC_LITERAL(11, 102, 12), // "slotcombobox"
-QT_MOC_LITERAL(12, 115, 5), // "index"
-QT_MOC_LITERAL(13, 121, 20), // "quitter_windowreseau"
-QT_MOC_LITERAL(14, 142, 15) // "valider_adresse"
+QT_MOC_LITERAL(5, 39, 4), // "next"
+QT_MOC_LITERAL(6, 44, 8), // "previous"
+QT_MOC_LITERAL(7, 53, 11), // "onShowFrame"
+QT_MOC_LITERAL(8, 65, 5), // "frame"
+QT_MOC_LITERAL(9, 71, 8), // "position"
+QT_MOC_LITERAL(10, 80, 12), // "onLineReturn"
+QT_MOC_LITERAL(11, 93, 8), // "timecode"
+QT_MOC_LITERAL(12, 102, 13), // "onSliderMoved"
+QT_MOC_LITERAL(13, 116, 12), // "slotcombobox"
+QT_MOC_LITERAL(14, 129, 5), // "index"
+QT_MOC_LITERAL(15, 135, 20), // "quitter_windowreseau"
+QT_MOC_LITERAL(16, 156, 15) // "valider_adresse"
 
     },
     "MainWindow\0showImageSignal\0\0play\0pause\0"
-    "onShowFrame\0frame\0position\0onLineReturn\0"
-    "timecode\0onSliderMoved\0slotcombobox\0"
-    "index\0quitter_windowreseau\0valider_adresse"
+    "next\0previous\0onShowFrame\0frame\0"
+    "position\0onLineReturn\0timecode\0"
+    "onSliderMoved\0slotcombobox\0index\0"
+    "quitter_windowreseau\0valider_adresse"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +62,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+      11,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,17 +70,19 @@ static const uint qt_meta_data_MainWindow[] = {
        1,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   59,    2, 0x06 /* Public */,
+       1,    1,   69,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       3,    0,   62,    2, 0x0a /* Public */,
-       4,    0,   63,    2, 0x0a /* Public */,
-       5,    2,   64,    2, 0x0a /* Public */,
-       8,    1,   69,    2, 0x0a /* Public */,
-      10,    1,   72,    2, 0x0a /* Public */,
-      11,    1,   75,    2, 0x0a /* Public */,
-      13,    0,   78,    2, 0x0a /* Public */,
-      14,    0,   79,    2, 0x0a /* Public */,
+       3,    0,   72,    2, 0x0a /* Public */,
+       4,    0,   73,    2, 0x0a /* Public */,
+       5,    0,   74,    2, 0x0a /* Public */,
+       6,    0,   75,    2, 0x0a /* Public */,
+       7,    2,   76,    2, 0x0a /* Public */,
+      10,    1,   81,    2, 0x0a /* Public */,
+      12,    1,   84,    2, 0x0a /* Public */,
+      13,    1,   87,    2, 0x0a /* Public */,
+      15,    0,   90,    2, 0x0a /* Public */,
+      16,    0,   91,    2, 0x0a /* Public */,
 
  // signals: parameters
     QMetaType::Void, QMetaType::QImage,    2,
@@ -85,10 +90,12 @@ static const uint qt_meta_data_MainWindow[] = {
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::VoidStar, QMetaType::UInt,    6,    7,
-    QMetaType::Void, QMetaType::QString,    9,
-    QMetaType::Void, QMetaType::Int,    9,
-    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::VoidStar, QMetaType::UInt,    8,    9,
+    QMetaType::Void, QMetaType::QString,   11,
+    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void, QMetaType::Int,   14,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -104,12 +111,14 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 0: _t->showImageSignal((*reinterpret_cast< QImage(*)>(_a[1]))); break;
         case 1: _t->play(); break;
         case 2: _t->pause(); break;
-        case 3: _t->onShowFrame((*reinterpret_cast< void*(*)>(_a[1])),(*reinterpret_cast< uint(*)>(_a[2]))); break;
-        case 4: _t->onLineReturn((*reinterpret_cast< QString(*)>(_a[1]))); break;
-        case 5: _t->onSliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 6: _t->slotcombobox((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 7: _t->quitter_windowreseau(); break;
-        case 8: _t->valider_adresse(); break;
+        case 3: _t->next(); break;
+        case 4: _t->previous(); break;
+        case 5: _t->onShowFrame((*reinterpret_cast< void*(*)>(_a[1])),(*reinterpret_cast< uint(*)>(_a[2]))); break;
+        case 6: _t->onLineReturn((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 7: _t->onSliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->slotcombobox((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->quitter_windowreseau(); break;
+        case 10: _t->valider_adresse(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -150,13 +159,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 11)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 11;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 11)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 11;
     }
     return _id;
 }
